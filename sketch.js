a7ef67d67f7c5063a6a560aa5e9266f7b43aa006
hidiksha <76519868+hidiksha@users.noreply.github.com>
2021-03-03 17:18:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas,bgimg;
+var gameState=0;
+
+var detabase,playerCount,form,game,player;
+function setup(){
+    database= firebase.database();
+    createCanvas(500,500);
+   game=new Game();
+   game.getState();
+   game.start();
+
+}
+
+function draw(){
+   if(playerCount===4){
+       game.update(1)
+   }
+   if(gameState===1){
+clear();
+game.play();
+
+   }
+}     
+
+
+
